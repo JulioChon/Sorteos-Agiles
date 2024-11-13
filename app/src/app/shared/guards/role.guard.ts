1fb637acd887820bc6f23803621e238697c42a04
Daniel Armando Peña García <135689456+DanielPenaGarcia@users.noreply.github.com>
2024-11-12 18:04:35 -0700

    Update role.guard.ts
    Guard para admins

--- app/src/app/shared/guards/role.guard.ts
+++ app/src/app/shared/guards/role.guard.ts
@@ -1,5 +1,13 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '@shared/services/auth/auth.service';
 
 export const roleGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const user = authService.getUser();
+  if (user  && !user.admin) {
+    return router.parseUrl('/');
+  }
   return true;
 };
