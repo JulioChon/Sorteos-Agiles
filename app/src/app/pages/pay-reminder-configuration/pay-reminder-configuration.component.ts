3f829601254e0bc7d26f3bd36a84a4733b58266c
Daniel Armando Peña García <135689456+DanielPenaGarcia@users.noreply.github.com>
2024-11-12 22:51:09 -0700

    Guardar configuracion

--- app/src/app/pages/pay-reminder-configuration/pay-reminder-configuration.component.ts
+++ app/src/app/pages/pay-reminder-configuration/pay-reminder-configuration.component.ts
@@ -2,7 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
+import { PayReminderConfigurationService } from './pay-reminder-configuration.service';
+import { ConfiguracionRecordatorioPago } from './pay-reminder-configuration.types';
+import { AlertService } from '@shared/services/alert.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pay-reminder-configuration',
@@ -15,20 +26,79 @@ import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angul
     ReactiveFormsModule,
   ],
   templateUrl: './pay-reminder-configuration.component.html',
-  styleUrl: './pay-reminder-configuration.component.scss',
+  styleUrls: ['./pay-reminder-configuration.component.scss'],
+  providers: [PayReminderConfigurationService],
 })
 export class PayReminderConfigurationComponent implements OnInit {
-
   reminderForm: FormGroup;
 
   constructor(
     private readonly formBuilder: FormBuilder,
+    private readonly payReminderConfigurationService: PayReminderConfigurationService,
+    private readonly alertService: AlertService,
+    private readonly router: Router
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initReminderForm();
+  }
 
   initReminderForm(): void {
-    this.reminderForm = this.formBuilder.group({
-    });
+    this.reminderForm = this.formBuilder.group(
+      {
+        days: [0, [Validators.min(0)]],
+        hours: [0, [Validators.min(0), Validators.max(23)]],
+        minutes: [0, [Validators.min(0), Validators.max(59)]],
+        seconds: [0, [Validators.min(0), Validators.max(59)]],
+      },
+      { validators: [this.atLeastOneTimeFieldValidator] } // Validator a nivel de formulario
+    );
+  }
+
+  atLeastOneTimeFieldValidator(form: AbstractControl) {
+    const { days, hours, minutes, seconds } = form.value;
+    if (days > 0 || hours > 0 || minutes > 0 || seconds > 0) {
+      return null;
+    }
+    return { atLeastOne: true };
+  }
+
+  onSubmit(): void {
+    if (this.reminderForm.invalid) {
+      this.reminderForm.markAllAsTouched();
+      return;
+    }
+    const configuracion : ConfiguracionRecordatorioPago = {
+      dias: this.days.value,
+      horas: this.hours.value,
+      minutos: this.minutes.value,
+      segundos: this.seconds.value
+    }
+    this.payReminderConfigurationService.createConfiguration(configuracion).subscribe(
+      () => {
+        this.alertService.openInfoModal('Configuración guardada correctamente');
+        this.router.navigate(['/admin']);
+      },
+      (error) => {
+        this.alertService.openInfoModal('Error al guardar la configuración');
+      }
+    );
+  }
+
+  // Getters para los campos del formulario
+  get days() {
+    return this.reminderForm.get('days');
+  }
+
+  get hours() {
+    return this.reminderForm.get('hours');
+  }
+
+  get minutes() {
+    return this.reminderForm.get('minutes');
+  }
+
+  get seconds() {
+    return this.reminderForm.get('seconds');
   }
 }
